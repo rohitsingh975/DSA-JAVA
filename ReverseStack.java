1baf835ac348b2f7589a6b2785aaae6abaa2d913
Rohit Singh <engineer.rohit8@gmail.com>
2022-01-31 00:16:17 +0530

    Stack Assignement-1

---
+++ ReverseStack.java
@@ -0,0 +1,49 @@
+package com.company;
+
+
+import java.util.Stack;
+
+public class ReverseStack {
+    static Stack<Character> st = new Stack<>();
+
+    static void pushBottom(char y) {
+        if (st.isEmpty())
+            st.push(y);
+        else {
+            char x = st.peek();
+            st.pop();
+            pushBottom(y);
+            st.push(x);
+        }
+    }
+
+    static void reverse() {
+        if (st.size() > 0) {
+            char y = st.peek();
+            st.pop();
+            reverse();
+            pushBottom(y);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        st.push('1');
+        st.push('2');
+        st.push('3');
+        st.push('4');
+        System.out.println("Original Stack");
+
+        System.out.println(st);
+
+        // function to reverse
+        // the stack
+        reverse();
+
+        System.out.println("Reversed Stack");
+
+        System.out.println(st);
+    }
+
+
+}
