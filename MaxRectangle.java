30d565c01b029d48eac9f9d4d463f1075df79e19
Rohit Singh <sd5@consultlane.com>
2022-02-10 00:00:05 +0530

    Stack Assignment-2

--- MaxRectangle.java
+++ MaxRectangle.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class MaxRectangle {
     public static void main(String[] args) {
@@ -16,19 +17,62 @@ public class MaxRectangle {
     public static int maximalRectangle(char[][] matrix) {
         int row = matrix.length;
         int column = matrix[0].length;
+        int maxArea = Integer.MIN_VALUE;
+
+        int[][] intMatrix = new int[row][column];
 
         //Transforming given binary matrix to matrix of size of histogram
-        for (int c = 0; c < column; c++) {
-            for (int i = 1; i < row; i++) {
-                if (matrix[i][c] != '0') {
-                    matrix[i][c] = (char) (1 + (int) (matrix[i - 1][c]));
+        for (int i = 0; i < column; i++) {
+            for (int j = 0; j < row; j++) {
+                if (j == 0) {
+                    intMatrix[0][i] = matrix[0][i] == '0' ? 0 : 1;
+                    continue;
                 }
+                intMatrix[j][i] = matrix[j][i] == '0' ? 0 : intMatrix[j - 1][i] + 1;
             }
         }
+        //System.out.println(Arrays.deepToString(intMatrix));
 
-        
-        System.out.println(Arrays.deepToString(matrix));
-        return 0;
+        for (int r = row - 1; r >= 0; r--) {
+            int n = intMatrix[r].length;
+
+            int[] nse = new int[n];
+            int[] pse = new int[n];
+            Stack<Integer> st = new Stack<>();
+
+            st.push(0);
+            for (int i = 1; i < n; i++) {
+                while (!st.empty() && intMatrix[r][i] < intMatrix[r][st.peek()]) {
+                    nse[st.peek()] = i;
+                    st.pop();
+                }
+                st.push(i);
+            }
+
+            while (!st.empty()) {
+                nse[st.peek()] = n;
+                st.pop();
+            }
+
+            st.push(n - 1);
+            for (int i = n - 2; i >= 0; i--) {
+                while (!st.empty() && intMatrix[r][i] < intMatrix[r][st.peek()]) {
+                    pse[st.peek()] = i;
+                    st.pop();
+                }
+                st.push(i);
+            }
+
+            while (!st.empty()) {
+                pse[st.peek()] = -1;
+                st.pop();
+            }
+
+            for (int i = 0; i < n; i++) {
+                maxArea = Math.max(maxArea, intMatrix[r][i] * (nse[i] - pse[i] - 1));
+            }
+        }
+        return maxArea;
     }
 
 }
